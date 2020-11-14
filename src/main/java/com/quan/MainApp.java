3685d4ae84f6386ba285b41f36d645b8724630fb
XieXinQuan <xiexinquan520@163.com>
2020-11-14 17:34:41 +0800

    create project

---
+++ src/main/java/com/quan/MainApp.java
@@ -0,0 +1,16 @@
+package com.quan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: xiexinquan520@163.com
+ * User: XieXinQuan
+ * DATE:2020/11/14
+ */
+@SpringBootApplication
+public class MainApp {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApp.class, args);
+    }
+}
